384ff375d62c27c9bf510149c17ae6798bea9001
Кирилл Щелухин <71601556+Kir-Scheluh@users.noreply.github.com>
2023-05-17 10:01:41 +0300

    Задание 1
    Запоминается группа, номер в группе и любимый фильм с помощью «getSharedPreferences»

--- app/src/main/java/com/mirea/schelukhinka/lesson6/MainActivity.java
+++ app/src/main/java/com/mirea/schelukhinka/lesson6/MainActivity.java
@@ -2,13 +2,39 @@ package com.mirea.schelukhinka.lesson6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+
+import com.mirea.schelukhinka.lesson6.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding	= ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        SharedPreferences preferences	= getSharedPreferences("mirea_settings",	Context.MODE_PRIVATE);
+        String group = preferences.getString("Group", "");
+        int numberInList = preferences.getInt("NumberInList", 0);
+        String favoriteFilm = preferences.getString("Film", "");
+
+        binding.editTextTextGroup.setText(group);
+        binding.editTextNumberList.setText(String.valueOf(numberInList));
+        binding.editTextTextFilm.setText(favoriteFilm);
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences preferences_bt = getSharedPreferences("mirea_settings",	Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor_bt	= preferences_bt.edit();
+                editor_bt.putString("Group", binding.editTextTextGroup.getText().toString());
+                editor_bt.putInt("NumberInList", Integer.parseInt(binding.editTextNumberList.getText().toString()));
+                editor_bt.putString("Film",binding.editTextTextFilm.getText().toString());
+                editor_bt.apply();
+            }
+        });
     }
 }
